f7f60dc1202d3332df168cd0ddf4cd0821a163f2
Enrico Seiler <enrico.seiler@hotmail.de>
2023-12-12 17:14:17 +0100

    [INFRA] Add linting

--- scripts/get_data_sizes.py
+++ scripts/get_data_sizes.py
@@ -1,3 +1,6 @@
+#!/usr/bin/env python
+"""Generates CSV files with data sizes from all.sizes."""
+
 ## INPUT:
 # 0         1           2       3               4       5               6           7                           8
 # KEY	    SUBKEY	    LEVEL	BIT_SIZE	    IBFS	AVG_LOAD_FACTOR	TBS_TOO_BIG	AVG_TBS_TOO_BIG_ELEMENTS	AVG_MAX_ELEMENTS
@@ -12,48 +15,85 @@
 # [Level 3]
 # [Avg Load Factor]
 
-import os
 import csv
+import os
 
-from shared import input_sizes, output_sizes
+from shared import input_sizes, output_sizes, write_csvs
 
-keys = ['k', 'alpha', 'hash', 'r-fpr', 'none', 'U+R', 'U']
-repeat = 3
 
-try:
-    os.makedirs(output_sizes, exist_ok=True)
-    print("Directory %s created" % output_sizes)
-except OSError:
-    print("files replaced in directory %s" % output_sizes)
+def create_size_csvs():
+    """Generates CSV files with data sizes from all.sizes."""
+    keys = ["k", "alpha", "hash", "r-fpr", "none", "U+R", "U"]
+    try:
+        os.makedirs(output_sizes, exist_ok=True)
+        print(f"Directory {output_sizes} created")
+    except OSError:
+        print("files replaced in directory {output_sizes}")
+    for key in keys:
+        output_path = f"{output_sizes}/{key}.csv"
+        subkeys = []
+        bit_size_levels = [[] for _ in range(4)]
+        avg_load_factor_levels = [[] for _ in range(4)]
+        with open(input_sizes, "r", encoding="utf-8") as file:
+            reader = csv.reader(file, delimiter="\t")
+            expected_level_counter = 0
+            for line in reader:
+                if key in {line[0], line[1]}:
+                    if int(line[2]) != expected_level_counter:
+                        for i in range(expected_level_counter, 4):
+                            avg_load_factor_levels[i].append(0)
+                            bit_size_levels[i].append(0)
+                        expected_level_counter = 0
+                    if line[1] == key and expected_level_counter == 0:
+                        subkeys.append(float(line[0].split("=")[1]))
+                    elif expected_level_counter == 0:
+                        subkeys.append(float(line[1].split("=")[1]))
+                    bit_size_levels[int(line[2])].append(round(float(line[3]) / (8 * 1024 * 1024 * 1025), 2))
+                    avg_load_factor_levels[int(line[2])].append(round(float(line[5]) / 100, 2))
+                    expected_level_counter += 1
+                    if expected_level_counter == 4:
+                        expected_level_counter = 0
+            for i in range(expected_level_counter, 4):
+                avg_load_factor_levels[i].append(0)
+                bit_size_levels[i].append(0)
+        (
+            subkeys,
+            bit_size_level_0,
+            bit_size_level_1,
+            bit_size_level_2,
+            bit_size_level_3,
+            avg_load_factor_level_0,
+            avg_load_factor_level_1,
+            avg_load_factor_level_2,
+            avg_load_factor_level_3,
+        ) = zip(
+            *sorted(
+                zip(
+                    subkeys,
+                    bit_size_levels[0],
+                    bit_size_levels[1],
+                    bit_size_levels[2],
+                    bit_size_levels[3],
+                    avg_load_factor_levels[0],
+                    avg_load_factor_levels[1],
+                    avg_load_factor_levels[2],
+                    avg_load_factor_levels[3],
+                )
+            )
+        )
+        all_lists = [
+            subkeys,
+            bit_size_level_0,
+            bit_size_level_1,
+            bit_size_level_2,
+            bit_size_level_3,
+            avg_load_factor_level_0,
+            avg_load_factor_level_1,
+            avg_load_factor_level_2,
+            avg_load_factor_level_3,
+        ]
+        write_csvs(output_path, all_lists)
 
-for key in keys:
-    output_path = f'{output_sizes}/{key}.csv'
-    subkeys = []
-    bit_size_levels = [[] for _ in range(4)]
-    avg_load_factor_levels = [[] for _ in range(4)]
-    with open(input_sizes, "r") as file:
-        reader = csv.reader(file, delimiter='\t')
-        expected_level_counter = 0
-        for line in reader:
-            if line[1] == key or line[0] == key:
-                if int(line[2]) != expected_level_counter:
-                    for i in range(expected_level_counter, 4):
-                        avg_load_factor_levels[i].append(0)
-                        bit_size_levels[i].append(0)
-                    expected_level_counter = 0
-                if line[1] == key and expected_level_counter == 0: subkeys.append(float(line[0].split('=')[1]))
-                elif expected_level_counter == 0: subkeys.append(float(line[1].split('=')[1]))
-                bit_size_levels[int(line[2])].append(round(float(line[3]) / (8*1024*1024*1025), 2))
-                avg_load_factor_levels[int(line[2])].append(round(float(line[5])/100, 2))
-                expected_level_counter += 1
-                if expected_level_counter == 4: expected_level_counter = 0
-        for i in range(expected_level_counter, 4):
-            avg_load_factor_levels[i].append(0)
-            bit_size_levels[i].append(0)
 
-    subkeys, bit_size_level_0, bit_size_level_1, bit_size_level_2, bit_size_level_3, avg_load_factor_level_0, avg_load_factor_level_1, avg_load_factor_level_2, avg_load_factor_level_3  = zip(*sorted(zip(subkeys, bit_size_levels[0], bit_size_levels[1], bit_size_levels[2], bit_size_levels[3], avg_load_factor_levels[0], avg_load_factor_levels[1], avg_load_factor_levels[2], avg_load_factor_levels[3])))
-    all_lists = [subkeys,bit_size_level_0, bit_size_level_1, bit_size_level_2, bit_size_level_3, avg_load_factor_level_0, avg_load_factor_level_1, avg_load_factor_level_2, avg_load_factor_level_3]
-    with open(output_path, "w", newline='') as file:
-        writer = csv.writer(file)
-        for list_ in all_lists:
-            writer.writerow(list_)
+if __name__ == "__main__":
+    create_size_csvs()
--- scripts/get_data_timings.py
+++ scripts/get_data_timings.py
@@ -1,3 +1,6 @@
+#!/usr/bin/env python
+"""Generates CSV files with data timings from query.timings."""
+
 ## INPUT:
 # 0         1       2                           3                           4                   5                                   6                           7                       8                                           9                                   10                                  11                          12                                          13                              14              15          16                  17                  18      19
 # KEY	    SUBKEY	wall_clock_time_in_seconds	peak_memory_usage_in_KiB	index_size_in_KiB	determine_query_length_in_seconds	query_file_io_in_seconds	load_index_in_seconds	compute_minimiser_avg_per_thread_in_seconds	compute_minimiser_sum_in_seconds	query_ibf_avg_per_thread_in_seconds	query_ibf_sum_in_seconds	generate_results_avg_per_thread_in_seconds	generate_results_sum_in_seconds	time-v-seconds	time-v-time	time-v-memory-kbits	time-v-memory-human	command -
@@ -14,83 +17,131 @@
 # [Determine Query Length]
 # [Query File IO]
 
-import os
 import csv
+import os
 
-from shared import input_timings, output_timings
-
-keys = ['k', 'alpha', 'hash', 'r-fpr', 'none', 'U+R', 'U']
-repeat = 3
-
-try:
-    os.makedirs(output_timings, exist_ok=True)
-    print("Directory %s created" % output_timings)
-except OSError:
-    print("files replaced in directory %s" % output_timings)
-
-for key in keys:
-    output_path = f'{output_timings}/{key}.csv'
-    subkeys = []
-    wall_clock_time_in_seconds = []
-    query_ibf_avg_per_thread_in_seconds = []
-    load_index_in_seconds = []
-    generate_results_avg_per_thread_in_seconds = []
-    compute_minimiser_avg_per_thread_in_seconds = []
-    determine_query_length_in_seconds = []
-    query_file_io_in_seconds = []
-
-    with open(input_timings, "r") as file:
-        reader = csv.reader(file, delimiter='\t')
-        i = 0
-        wall_clock_time_in_seconds_i = 0
-        query_ibf_avg_per_thread_in_seconds_i = 0
-        load_index_in_seconds_i = 0
-        generate_results_avg_per_thread_in_seconds_i = 0
-        compute_minimiser_avg_per_thread_in_seconds_i = 0
-        determine_query_length_in_seconds_i = 0
-        query_file_io_in_seconds_i = 0
-        for line in reader:
-            if line[0] == key or line[1] == key:
-                wall_clock_time_in_seconds_i += float(line[2])
-                query_ibf_avg_per_thread_in_seconds_i += float(line[10])
-                load_index_in_seconds_i += float(line[7])
-                generate_results_avg_per_thread_in_seconds_i += float(line[12])
-                compute_minimiser_avg_per_thread_in_seconds_i += float(line[8])
-                determine_query_length_in_seconds_i += float(line[5])
-                query_file_io_in_seconds_i += float(line[6])
-                i += 1
-                if i == repeat:
-                    if line[1] == key: subkeys.append(float(line[0].split('=')[1]))
-                    else: subkeys.append(float(line[1].split('=')[1]))
-                    query_ibf_avg_per_thread_in_seconds.append(round((query_ibf_avg_per_thread_in_seconds_i / repeat), 2))
-                    load_index_in_seconds.append(round((load_index_in_seconds_i / repeat), 2))
-                    generate_results_avg_per_thread_in_seconds.append(round((generate_results_avg_per_thread_in_seconds_i / repeat), 2))
-                    compute_minimiser_avg_per_thread_in_seconds.append(round((compute_minimiser_avg_per_thread_in_seconds_i / repeat), 2))
-                    determine_query_length_in_seconds.append(round((determine_query_length_in_seconds_i / repeat), 2))
-                    query_file_io_in_seconds.append(round((query_file_io_in_seconds_i / repeat), 2))
-                    wall_clock_time_in_seconds.append(round(max(0,(
-                                                                    wall_clock_time_in_seconds_i -
-                                                                    query_ibf_avg_per_thread_in_seconds_i -
-                                                                    load_index_in_seconds_i -
-                                                                    generate_results_avg_per_thread_in_seconds_i -
-                                                                    compute_minimiser_avg_per_thread_in_seconds_i -
-                                                                    determine_query_length_in_seconds_i -
-                                                                    query_file_io_in_seconds_i
-                                                                )) / repeat, 2))
-                    wall_clock_time_in_seconds_i = 0
-                    query_ibf_avg_per_thread_in_seconds_i = 0
-                    load_index_in_seconds_i = 0
-                    generate_results_avg_per_thread_in_seconds_i = 0
-                    compute_minimiser_avg_per_thread_in_seconds_i = 0
-                    determine_query_length_in_seconds_i = 0
-                    query_file_io_in_seconds_i = 0
-                    i = 0
+from shared import input_timings, output_timings, write_csvs
 
 
-    subkeys, wall_clock_time_in_seconds, query_ibf_avg_per_thread_in_seconds, load_index_in_seconds, generate_results_avg_per_thread_in_seconds, compute_minimiser_avg_per_thread_in_seconds, determine_query_length_in_seconds, query_file_io_in_seconds = zip(*sorted(zip(subkeys, wall_clock_time_in_seconds, query_ibf_avg_per_thread_in_seconds, load_index_in_seconds, generate_results_avg_per_thread_in_seconds, compute_minimiser_avg_per_thread_in_seconds, determine_query_length_in_seconds, query_file_io_in_seconds)))
+def create_timing_csv():
+    """Generates CSV files with data timings from query.timings."""
+    keys = ["k", "alpha", "hash", "r-fpr", "none", "U+R", "U"]
+    repeat = 3
+    try:
+        os.makedirs(output_timings, exist_ok=True)
+        print(f"Directory {output_timings} created")
+    except OSError:
+        print(f"files replaced in directory {output_timings}")
+    for key in keys:
+        output_path = f"{output_timings}/{key}.csv"
+        subkeys = []
+        wall_clock_time_in_seconds = []
+        query_ibf_avg_per_thread_in_seconds = []
+        load_index_in_seconds = []
+        generate_results_avg_per_thread_in_seconds = []
+        compute_minimiser_avg_per_thread_in_seconds = []
+        determine_query_length_in_seconds = []
+        query_file_io_in_seconds = []
+        with open(input_timings, "r", encoding="utf-8") as file:
+            reader = csv.reader(file, delimiter="\t")
+            i = 0
+            wall_clock_time_in_seconds_i = 0
+            query_ibf_avg_per_thread_in_seconds_i = 0
+            load_index_in_seconds_i = 0
+            generate_results_avg_per_thread_in_seconds_i = 0
+            compute_minimiser_avg_per_thread_in_seconds_i = 0
+            determine_query_length_in_seconds_i = 0
+            query_file_io_in_seconds_i = 0
+            for line in reader:
+                if key in {line[0], line[1]}:
+                    wall_clock_time_in_seconds_i += float(line[2])
+                    query_ibf_avg_per_thread_in_seconds_i += float(line[10])
+                    load_index_in_seconds_i += float(line[7])
+                    generate_results_avg_per_thread_in_seconds_i += float(line[12])
+                    compute_minimiser_avg_per_thread_in_seconds_i += float(line[8])
+                    determine_query_length_in_seconds_i += float(line[5])
+                    query_file_io_in_seconds_i += float(line[6])
+                    i += 1
+                    if i == repeat:
+                        if line[1] == key:
+                            subkeys.append(float(line[0].split("=")[1]))
+                        else:
+                            subkeys.append(float(line[1].split("=")[1]))
+                        query_ibf_avg_per_thread_in_seconds.append(
+                            round((query_ibf_avg_per_thread_in_seconds_i / repeat), 2)
+                        )
+                        load_index_in_seconds.append(round((load_index_in_seconds_i / repeat), 2))
+                        generate_results_avg_per_thread_in_seconds.append(
+                            round((generate_results_avg_per_thread_in_seconds_i / repeat), 2)
+                        )
+                        compute_minimiser_avg_per_thread_in_seconds.append(
+                            round((compute_minimiser_avg_per_thread_in_seconds_i / repeat), 2)
+                        )
+                        determine_query_length_in_seconds.append(
+                            round((determine_query_length_in_seconds_i / repeat), 2)
+                        )
+                        query_file_io_in_seconds.append(round((query_file_io_in_seconds_i / repeat), 2))
+                        wall_clock_time_in_seconds.append(
+                            round(
+                                max(
+                                    0,
+                                    (
+                                        wall_clock_time_in_seconds_i
+                                        - query_ibf_avg_per_thread_in_seconds_i
+                                        - load_index_in_seconds_i
+                                        - generate_results_avg_per_thread_in_seconds_i
+                                        - compute_minimiser_avg_per_thread_in_seconds_i
+                                        - determine_query_length_in_seconds_i
+                                        - query_file_io_in_seconds_i
+                                    ),
+                                )
+                                / repeat,
+                                2,
+                            )
+                        )
+                        wall_clock_time_in_seconds_i = 0
+                        query_ibf_avg_per_thread_in_seconds_i = 0
+                        load_index_in_seconds_i = 0
+                        generate_results_avg_per_thread_in_seconds_i = 0
+                        compute_minimiser_avg_per_thread_in_seconds_i = 0
+                        determine_query_length_in_seconds_i = 0
+                        query_file_io_in_seconds_i = 0
+                        i = 0
+        (
+            subkeys,
+            wall_clock_time_in_seconds,
+            query_ibf_avg_per_thread_in_seconds,
+            load_index_in_seconds,
+            generate_results_avg_per_thread_in_seconds,
+            compute_minimiser_avg_per_thread_in_seconds,
+            determine_query_length_in_seconds,
+            query_file_io_in_seconds,
+        ) = zip(
+            *sorted(
+                zip(
+                    subkeys,
+                    wall_clock_time_in_seconds,
+                    query_ibf_avg_per_thread_in_seconds,
+                    load_index_in_seconds,
+                    generate_results_avg_per_thread_in_seconds,
+                    compute_minimiser_avg_per_thread_in_seconds,
+                    determine_query_length_in_seconds,
+                    query_file_io_in_seconds,
+                )
+            )
+        )
+        all_lists = [
+            subkeys,
+            query_file_io_in_seconds,
+            determine_query_length_in_seconds,
+            compute_minimiser_avg_per_thread_in_seconds,
+            generate_results_avg_per_thread_in_seconds,
+            load_index_in_seconds,
+            query_ibf_avg_per_thread_in_seconds,
+            wall_clock_time_in_seconds,
+        ]
+        write_csvs(output_path, all_lists)
 
-    all_lists = [subkeys, query_file_io_in_seconds, determine_query_length_in_seconds, compute_minimiser_avg_per_thread_in_seconds, generate_results_avg_per_thread_in_seconds, load_index_in_seconds, query_ibf_avg_per_thread_in_seconds, wall_clock_time_in_seconds]
-    with open(output_path, "w", newline='') as file:
-        writer = csv.writer(file)
-        for list_ in all_lists:
-            writer.writerow(list_)
+
+if __name__ == "__main__":
+    create_timing_csv()
