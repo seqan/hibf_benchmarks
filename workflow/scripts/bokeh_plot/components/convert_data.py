719b829671c27ac5d0a4602095ef95411960d576
leoxnard <leoxnard@users.noreply.github.com>
2024-07-23 17:29:40 +0200

    [MISC] Merge changes
    Co-authored-by: Enrico Seiler <enrico.seiler@hotmail.de>

--- workflow/scripts/bokeh_plot/components/convert_data.py
+++ workflow/scripts/bokeh_plot/components/convert_data.py
@@ -1,33 +1,59 @@
 """Functions to convert the data for the plots."""
 
-from components.helpers import add_arrays, convert_list_to_float, devide_arrays_in_percentage
 
-
-def convert_time_data(data, key, time_format, time_offset):
+def prepare_time_data(time_data_list, keypair, time_configs):
     """Returns a dictionary containing the given data in the format for the time plot."""
-    result = {}
-    result["SUBKEY"] = [f"{key} = {value}" for value in data[0]]
-    result["value"] = data[0]
-    result["all_times"] = add_arrays(data[(1 + time_offset) :])
-    for i, element in enumerate(data[1:]):
-        result[time_format[i + 1]] = convert_list_to_float(element)
-        result[f"{time_format[i + 1]}_percentage"] = devide_arrays_in_percentage(
-            result[time_format[i + 1]], result["all_times"]
-        )
-    return result
+    key, keyname = keypair
+    data = time_data_list[time_data_list.loc[:, "KEY"] == key].copy()
+    key_value = data.iloc[:, 1].str.split("=").str[1].astype(float).tolist()
+    data["TOTAL_TIME"] = data.loc[:, time_configs["FORMAT"]].sum(axis=1)
+    for col in data.columns:
+        if "seconds" in col:
+            data.insert(
+                data.columns.get_loc(col) + 1,
+                col.replace("in_seconds", "percentage"),
+                (data[col] / data["TOTAL_TIME"] * 100).round(3),
+            )
+    data.loc[:, "KEY"] = keyname
+    data.insert(2, "SUBKEY_VALUE", key_value)
+    for name in time_configs["NAMES"]:
+        data.insert(data.columns.get_loc(name) + 1, time_configs["NAMES"][name], data[name])
+    data = data.sort_values(by=["SUBKEY_VALUE"])
+    # print("Available data for time plot:")
+    # print(data.columns)
+    export = data.to_dict(orient="list")
+    return export
 
 
-def convert_size_data(data, key, size_format):
+def prepare_size_data(size_data, keypair, size_configs):
     """Returns a dictionary containing the given data in the format for the size plot."""
-    result = {}
-    result[size_format[0]] = [f"{key} = {value}" for value in data[0]]
-    result["value"] = data[0]
-    result["sizes"] = [sum(float(i) for i in sublist) for sublist in zip(*data[1:5])]
-    for i, element in enumerate(data[1:5]):
-        result[size_format[i + 1]] = convert_list_to_float(element)
-        result[f"{size_format[i+1]}_percentage"] = devide_arrays_in_percentage(
-            result[size_format[i + 1]], result["sizes"]
-        )
-    for i, element in enumerate(data[5:]):
-        result[f"{size_format[i+1]}_avg_load_factor"] = convert_list_to_float(element)
-    return result
+    key, keyname = keypair
+    key_data = size_data[size_data.loc[:, "KEY"] == key].copy().sort_values(by=["SUBKEY", "LEVEL"])
+    key_data = key_data.reset_index(drop=True)
+    for i in range(4):
+        key_data.loc[-(i + 1)] = i
+    key_data.insert(5, "GB_SIZE", round((key_data["BIT_SIZE"].astype(float)) / (1000 * 1000 * 1000 * 8), 3))
+    data = key_data.pivot_table(
+        index="SUBKEY",
+        columns="LEVEL",
+        values=[col for col in key_data.columns if col not in ["KEY", "SUBKEY", "LEVEL", "SUBKEY_VALUE"]],
+        aggfunc="first",
+    )
+    data = data.drop([0, 1, 2, 3])
+    data.reset_index(inplace=True)
+    data.fillna(0, inplace=True)
+    data.columns = [f"LEVEL_{level}_{value}" if value != "SUBKEY" else value for (value, level) in data.columns]
+    data["GB_TOTAL_SIZE"] = data.loc[:, size_configs["FORMAT"]].sum(axis=1)
+    for col in data.columns:
+        if "GB_SIZE" in col:
+            data[col.replace("GB_SIZE", "GB_SIZE_percentage")] = (data[col] / data["GB_TOTAL_SIZE"] * 100).round(3)
+    subkey_value_list = data["SUBKEY"].str.split("=").str[1].astype(float).tolist()
+    data.insert(0, "SUBKEY_VALUE", subkey_value_list)
+    data.insert(0, "KEY", keyname)
+    for name in size_configs["NAMES"]:
+        data.insert(data.columns.get_loc(name) + 1, size_configs["NAMES"][name], data[name])
+    data = data.sort_values(by=["SUBKEY_VALUE"])
+    # print("Available data for size plot:")
+    # print(data.columns)
+    export = data.to_dict(orient="list")
+    return export
--- workflow/scripts/bokeh_plot/components/helpers.py
+++ workflow/scripts/bokeh_plot/components/helpers.py
@@ -1,12 +1,7 @@
 """Helper functions."""
 
 
-def convert_list_to_float(data):
-    """Returns a list containing the values of the given list as floats."""
-    return [float("nan") if float(i) == 0.0 else float(i) for i in data]
-
-
-def convert_list_to_string(data):
+def convert_dic_to_list(data):
     """Returns a list containing the values of the given list as strings."""
     return [str(i) for i in data]
 
@@ -18,9 +13,4 @@ def add_arrays(data):
 
 def get_max_result(data, factor):
     """Returns the maximum value of the given list multiplied by the given factor."""
-    return round(max(add_arrays(data)) * factor, 3)
-
-
-def devide_arrays_in_percentage(list1, list2):
-    """Returns the percentage of each element from list1 to list2."""
-    return [round(float(i) / float(j) * 100, 2) for i, j in zip(list1, list2)]
+    return round(max(data) * factor, 3)
